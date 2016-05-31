84f51ca2b45adf4c8cbcdff21a53a7ccfbc4c108
Ponto Mega <contato@pontomega.com.br>
2016-05-31 17:51:54 -0300

    Changed P to DIV tag

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,13 +1,22 @@
 <?php
+
 /**
+
  * @copyright	Copyright (c) 2015 Ponto mega. All rights reserved.
+
  * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+
  */
 
+
+
 // no direct access
+
 defined('_JEXEC') or die;
 defined('_JEXEC') or die;
 JHtml::_('behavior.modal');
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+
 
 require_once(JPATH_ADMINISTRATOR . '/components/com_jce/helpers/browser.php');
 
@@ -16,5 +25,6 @@ $language->load('com_jce', JPATH_ADMINISTRATOR);
 $icon = 'header/icon-48-media.png';
 
 $link = WFBrowserHelper::getBrowserLink('', $filter);
-echo '<p><a href="'.$link.'" class="modal button btn btn-small" rel="{handler: \'iframe\', size: {x: 875, y: 550}, onClose: function() {}}">'.'<i class="icon-pictures"></i>'.JText::_('WF_QUICKICON_BROWSER').'</a></p>';
+echo '<div class="'.$class_sfx.'"><a href="'.$link.'" class="modal button btn btn-small" rel="{handler: \'iframe\', size: {x: 875, y: 550}, onClose: function() {}}">'.'<i class="icon-pictures"></i>'.JText::_('WF_QUICKICON_BROWSER').'</a></div>';
+
 ?>
